a2d9fc012d39469ef93b712510799728c2820821
WindomZ <git.windomz@gmail.com>
2017-06-02 01:36:12 +0800

    optimize build script

--- build.sh
+++ build.sh
@@ -11,13 +11,16 @@ docker rm -f free-idea
 docker build --rm=true \
 -t windomz/free-idea \
 --file ./crack/Dockerfile ./crack
-
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
+port=25252
+if [ -n "$(echo $1| sed -n "/^[0-9]\+$/p")" ];then
+  port=$1
+fi
 docker run --restart=always \
--p 25252:25252 \
+-p ${port}:25252 \
 --name free-idea -d windomz/free-idea
 
 rm ./crack/app.tar
